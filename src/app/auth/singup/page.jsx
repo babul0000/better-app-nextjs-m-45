a6edb10d224e26d418ad793990c7bf92033d7424
Your Name <babulhossan.info@gmail.com>
2026-04-22 00:36:30 +0600

    from

--- src/app/auth/singup/page.jsx
+++ src/app/auth/singup/page.jsx
@@ -1,9 +1,119 @@
+"use client";
 
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import {
+    Button,
+    Description,
+    FieldError,
+    Form,
+    Input,
+    Label,
+    TextField,
+} from "@heroui/react";
+import { object } from "better-auth";
 
 const SingUp = () => {
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const userData = Object.fromEntries(formData.entries());
+        console.log("form submitted", userData);
+
+        const {data, error } = await authClient.singUp.email({
+            name: "name", // required
+            email: "email", // required
+            password: "password1234",
+        });
+
+        console.log("singup response", {data, error});
+        
+
+        // const data = object.fromEntries(formData.entries());
+        // console.log('available', data);
+
+        //     const formData = new FormData(e.currentTarget);
+        //     const data = {};
+        //     // Convert FormData to plain object
+        //     formData.forEach((value, key) => {
+        //         data[key] = value.toString();
+        //     });
+        //     alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
+    };
+
     return (
         <div>
             <h1>SingUp</h1>
+
+            <div className="w-1/2 mx-auto">
+                <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+                    <TextField
+                        isRequired
+                        name="name"
+                        validate={(value) => {
+                            if (value.length < 3) {
+                                return "Name must be at least 3 characters";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Name</Label>
+                        <Input name="name" placeholder="John Doe" />
+                        <FieldError />
+                    </TextField>
+
+                    <TextField
+                        isRequired
+                        name="email"
+                        type="email"
+                        validate={(value) => {
+                            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                                return "Please enter a valid email address";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Email</Label>
+                        <Input name="email" placeholder="john@example.com" />
+                        <FieldError />
+                    </TextField>
+                    <TextField
+                        isRequired
+                        minLength={8}
+                        name="password"
+                        type="password"
+                        validate={(value) => {
+                            if (value.length < 8) {
+                                return "Password must be at least 8 characters";
+                            }
+                            if (!/[A-Z]/.test(value)) {
+                                return "Password must contain at least one uppercase letter";
+                            }
+                            if (!/[0-9]/.test(value)) {
+                                return "Password must contain at least one number";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Password</Label>
+                        <Input name="password" placeholder="Enter your password" />
+                        <Description>
+                            Must be at least 8 characters with 1 uppercase and 1 number
+                        </Description>
+                        <FieldError />
+                    </TextField>
+                    <div className="flex gap-2">
+                        <Button type="submit">
+                            <Check />
+                            Submit
+                        </Button>
+                        <Button type="reset" variant="secondary">
+                            Reset
+                        </Button>
+                    </div>
+                </Form>
+            </div>
         </div>
     );
 };
